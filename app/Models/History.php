5ec0c74028e8fc37e84b363d5e6d6d87417436a6
Genezareto <genezaretokaijeputraguntur@gmail.com>
2025-10-16 13:22:50 +0800

    History fillable

--- app/Models/History.php
+++ app/Models/History.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class History extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'lamp_id',
+        'status'
+    ];
 }
