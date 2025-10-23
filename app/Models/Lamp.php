395cebad58d0f25639212764d3461d01562871d0
Genezareto <genezaretokaijeputraguntur@gmail.com>
2025-10-23 14:35:13 +0800

    TableSeed

--- app/Models/Lamp.php
+++ app/Models/Lamp.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Lamp extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'status'
+    ];
+
+    public function histories()
+    {
+        return $this->hasMany(History::class);
+    }
 }
